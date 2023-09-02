fcf01aa722233735b9958d7b9ce8dbac97e70a08
Phoenix043 <132455515+Phoenix043@users.noreply.github.com>
2023-09-02 14:00:25 +0530

    Update settings.py

--- schoolmanagement/settings.py
+++ schoolmanagement/settings.py
@@ -16,7 +16,7 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
-STATIC_DIR=os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
 
 # Quick-start development settings - unsuitable for production
@@ -135,10 +135,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS=[
-STATIC_DIR,
- ]
 
 
 
